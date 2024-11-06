7c503833866b1f124a0e8828c9e46e1e2b463c44
masakiEbiko <ebiko@kyoei-.com>
2024-11-06 11:26:31 +0900

    Initial Commit

---
+++ Jyanken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jyanken
+//
+//  Created by mba2408.spacegray kyoei.engine on 2024/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
